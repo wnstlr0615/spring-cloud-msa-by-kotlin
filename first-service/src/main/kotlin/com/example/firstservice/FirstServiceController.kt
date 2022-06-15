4652abcdd76ffc58deba0b5fe178158817332597
joonsik <ryan0@kakao.com>
2022-06-15 21:40:49 +0900

    포트를 자동 할당으로 수정 및 기존 요청에 호출되는 포트 번호가 노출되도록 수정

--- first-service/src/main/kotlin/com/example/firstservice/FirstServiceController.kt
+++ first-service/src/main/kotlin/com/example/firstservice/FirstServiceController.kt
@@ -1,16 +1,22 @@
 package com.example.firstservice
 
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletRequest
 
 
 @RestController
 @RequestMapping("/first-service")
 class FirstServiceController {
-    val log = LoggerFactory.getLogger(FirstServiceController::class.java)
+    val log: Logger = LoggerFactory.getLogger(FirstServiceController::class.java)
+    @Autowired
+    lateinit var env : Environment
     @GetMapping("/welcome")
     fun welcome(): String{
         return "Welcome to the First service."
@@ -22,7 +28,8 @@ class FirstServiceController {
     }
 
     @GetMapping("/check")
-    fun check(): String{
-        return "Hi, there. this is a message from First Service"
+    fun check(request: HttpServletRequest): String{
+        log.info("Server port = {}", request.serverPort)
+        return "Hi, there. this is a message from First Service on PORT ${env.getProperty("local.server.port")}"
     }
 }
--- second-service/src/main/kotlin/com/example/secondservice/SecondServiceController.kt
+++ second-service/src/main/kotlin/com/example/secondservice/SecondServiceController.kt
@@ -2,16 +2,20 @@ package com.example.secondservice
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletRequest
 
 @RestController
 @RequestMapping("/second-service")
 class SecondServiceController {
     val log: Logger = LoggerFactory.getLogger(SecondServiceController::class.java)
-
+    @Autowired
+    lateinit var env : Environment
     @GetMapping("/welcome")
     fun welcome(): String{
         return "Welcome to the second service."
@@ -22,8 +26,9 @@ class SecondServiceController {
         return "Hello World in second Service."
     }
     @GetMapping("/check")
-    fun check(): String{
-        return "Hi, there. this is a message from Second Service"
+    fun check(request: HttpServletRequest): String{
+        log.info("Server port = {}", request.serverPort)
+        return "Hi, there. this is a message from Second Service on PORT ${env.getProperty("local.server.port")}"
     }
 
 }
