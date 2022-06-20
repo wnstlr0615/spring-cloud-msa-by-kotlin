44fbce1bd9dabb10125ddbe6f5ff21b1315a9342
joonsik <ryan0@kakao.com>
2022-06-20 20:38:42 +0900

    Add Rabbitmq를 통해 busrefresh 요청

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.cloud:spring-cloud-starter-bus-amqp")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
